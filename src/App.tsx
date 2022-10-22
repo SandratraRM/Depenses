c657a0018c9be79e43f13e621e93f2afe18d6174
Sandratra Rambeloson <srambeloson@wiser.com>
2022-10-22 17:19:27 +0300

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getLoggedUser, loginWithGoogle, logout } from "./google/auth";
+import { getIdToken, GoogleAuthProvider, User } from "firebase/auth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(): JSX.Element {
+    const [user, setUser] = useState<User | null>();
+    const [token, setToken] = useState<string>();
+    const [needLogin, setNeedLogin] = useState(false);
+    const inited = useRef(false);
+    const updateUser = () => {
+        getLoggedUser().then((user) => {
+            setUser(user);
+        });
+    };
+    useEffect(() => {
+        if (inited.current) {
+            return;
+        }
+        inited.current = true;
+        updateUser();
+    }, []);
+
+    useEffect(() => {
+        setNeedLogin(user === null);
+    }, [user]);
+    const handleLogin = () => {
+        loginWithGoogle().then((user) => {
+            if (user) {
+                setUser(user);
+            }
+        });
+    };
+    const handleLogout = async () => {
+        await logout();
+        updateUser();
+    };
+    return (
+        <>
+            {needLogin && <button onClick={handleLogin}>Login</button>}
+            {user && (
+                <div>
+                    <img referrerPolicy="no-referrer" src={user.photoURL as string} />
+                    <p>User name: {user.displayName}</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default App;
